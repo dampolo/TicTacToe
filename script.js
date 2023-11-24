440cdb7429d952675d4926de43cd3d6a4b717669
Damian Poloczek <damianpoloczek@gmail.com>
2023-11-24 19:27:57 +0100

    correction of the  animation

--- script.js
+++ script.js
@@ -37,9 +37,9 @@ function render() {
 function generateAnimatedCircle() {
     const svgCode = `
         <svg width="70" height="70" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="35" cy="35" r="30" fill="none" stroke="#00B0FF" stroke-width="5">
-                <animate attributeName="r" from="0" to="30" dur=".5s" begin="0s" fill="freeze" />
-                <animate attributeName="stroke-dasharray" values="0 0; 188 50" dur=".5s" begin="0s" fill="freeze" />
+            <circle cx="35" cy="35" r="50" fill="none" stroke="#00B0FF" stroke-width="5">
+                <animate attributeName="r" from="30" to="30" dur=".5s" begin="0s" fill="freeze" />
+                <animate attributeName="stroke-dasharray" values="0 0; 188 50" dur=".4s" begin="0s" fill="freeze" />
             </circle>
         </svg>
     `;
@@ -50,12 +50,13 @@ function generateAnimatedCircle() {
 function generateAnimatedCrossCircle() {
     const svgCode = `
         <svg width="80" height="80" xmlns="http://www.w3.org/2000/svg">
-
-            <line x1="15" y1="35" x2="55" y2="35" stroke="#FFC000" stroke-width="5">
-                <animate attributeName="x2" from="15" to="55" dur=".5s" begin="0s" fill="freeze" />
+            <line x1="20" y1="70" x2="60" y2="30" stroke="#FFC000" stroke-width="5">
+                <animate attributeName="x2" from="20" to="60" dur=".4s" begin="0s" fill="freeze" />
+                <animate attributeName="y2" from="70" to="30" dur=".4s" begin="0s" fill="freeze" />
             </line>
-            <line x1="35" y1="15" x2="35" y2="55" stroke="#FFC000" stroke-width="5">
-                <animate attributeName="y2" from="15" to="55" dur=".5s" begin="0s" fill="freeze" />
+            <line x1="20" y1="30" x2="60" y2="70" stroke="#FFC000" stroke-width="5">
+                <animate attributeName="x2" from="20" to="60" dur=".4s" begin="0s" fill="freeze" />
+                <animate attributeName="y2" from="30" to="70" dur=".4s" begin="0s" fill="freeze" />
             </line>
         </svg>
     `;
